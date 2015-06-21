85636409c882b701b9adf46878bcd9c957d8abc8
SaurabhPandey <saurabh.joy8@gmail.com>
2015-06-21 17:18:34 +0530

    CanvasText

--- app/src/main/java/com/example/saurabhpandey/scratchthescreen/SaurabhGraphics.java
+++ app/src/main/java/com/example/saurabhpandey/scratchthescreen/SaurabhGraphics.java
@@ -6,7 +6,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.view.View;
 
 /**
@@ -16,18 +16,32 @@ public class SaurabhGraphics extends View {
 
     Bitmap arrow, arrow2;
     float ChangingY, ChangingX;
+    Typeface opensans;
 
     public SaurabhGraphics(Context context) {
         super(context);
         arrow2 = BitmapFactory.decodeResource(getResources(), R.drawable.arrow2);
         arrow = BitmapFactory.decodeResource(getResources(), R.drawable.arrow);
         ChangingY = 0;
+        opensans = Typeface.createFromAsset(context.getAssets(), "OpenSans-ExtraBold.ttf");
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+//        Rect rect = new Rect();
+//        rect.set(0, 100, canvas.getWidth(), 100);
+//        Paint yellow = new Paint();
+//        yellow.setColor(Color.YELLOW);
+//        canvas.drawRect(rect, yellow);
         canvas.drawColor(Color.WHITE);
+        Paint textPaint = new Paint();
+        textPaint.setColor(Color.BLACK);
+        textPaint.setTextAlign(Paint.Align.CENTER);
+        textPaint.setTextSize(40);
+        canvas.drawText("The Moving Arrows ;) ", canvas.getWidth() / 2, canvas.getHeight() / 2, textPaint);
+
+
 
         canvas.drawBitmap(arrow, (canvas.getWidth() / 2 - 55), ChangingY, null);
 
@@ -48,11 +62,7 @@ public class SaurabhGraphics extends View {
 
             ChangingX = 0;
         }
-        Rect rect = new Rect();
-        rect.set(0, 100, canvas.getWidth(), 100);
-        Paint yellow = new Paint();
-        yellow.setColor(Color.YELLOW);
-        canvas.drawRect(rect, yellow);
+
         invalidate();
     }
 }
